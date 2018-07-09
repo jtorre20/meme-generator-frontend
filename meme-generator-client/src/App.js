95e6ad53302faa8a4383fae0f32bdb7caf489166
codingkasia <codingkasia@gmail.com>
2018-07-09 10:24:32 -0400

    changed fetch from 'search' to 'api fetch, styling, fetch 'post'request

--- meme-generator-client/src/App.js
+++ meme-generator-client/src/App.js
@@ -1,46 +1,178 @@
-import React, { Component } from 'react';
-import './App.css';
-import Search from './components/Search'
-import MemesList from './components/MemesList'
-const url = "https://api.imgflip.com/get_memes"
+import React from 'react';
+import './index.css';
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import MemesList from "./components/MemesList";
+import Meme from './components/Meme';
+const url = "https://api.imgflip.com/get_memes";
+const newMemeUrl = 'https://api.imgflip.com/caption_image'
 
-
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
-      searchTerm: "",
-      memes: []
-    };
+      memes: [],
+      myMemes: [{
+        height: 800,
+        id: "112126428",
+        name: "Distracted Boyfriend",
+        url: "https://i.imgflip.com/1ur9b0.jpg",
+        width: 1200
+      }],
+      memeLimit: 15,
+      addMeme: 10,
+      text0: '',
+      text1: ''
+    }
   }
 
   componentDidMount() {
-    this.fetchMemes();
+    this.fetchApi();
   }
 
-  fetchMemes = () => {
-    fetch(url)
-      .then(resp => resp.json())
-      .then(result => this.setState({ memes: result.data.memes }));
-        // 
-  };
-
-
-  updateTermSearch = (newSearchTerm) => {
-    this.setState({ searchTerm: newSearchTerm })
+  fetchApi = () => {
+    return fetch(url).then(resp => resp.json()).then(result => this.setState({ memes: result.data.memes })
+      // 
+    )
   }
 
-  filterMemes = () => {
-    return this.state.memes.filter(meme => meme.name.toLowerCase().includes(this.state.searchTerm))
-      // meme.name.toLowerCase().includes(this.state.searchTerm)
-  };
-  
+  fetchClickedMeme = (memeId) => {
+    fetch(newMemeUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json"
+      },
+      body: {
+        template_id: memeId,
+        username: "kasiarosenb",
+        password: "kasiarosenb",
+        text0: this.state.text0,
+        text1: this.state.text1
+      }
+    })
+      .then(response => response.json())
+      .then(result => console.log("posted clicked!", result));
+        // this.state.myMemes
+  }
+
   render() {
-    return <div className="App">
-        <Search updateTermSearch={this.updateTermSearch} />
-        <MemesList filterMemes={this.filterMemes()} />
+    return <div>
+        <h2>Create Your Own Meme!</h2>
+        <Meme myMemes={this.state.myMemes} />
+        <h4>
+          <i>Insert Some Text Below</i>
+        </h4>
+        <Form inline>
+          <FormGroup>
+            <ControlLabel>Top</ControlLabel> <FormControl type="text" onChange={event => this.setState(
+                  { text0: event.target.value }
+                )} />
+          </FormGroup> <FormGroup>
+            <ControlLabel>Bottom</ControlLabel> <FormControl type="text" onChange={event => this.setState(
+                  { text1: event.target.value }
+                )} />
+          </FormGroup>
+        </Form>
+        {this.state.memes
+          .slice(0, this.state.memeLimit)
+          .map((meme, index) => {
+            return <MemesList key={index} meme={meme} text0={this.state.text0} text1={this.state.text1} fetchClickedMeme={this.fetchClickedMeme} />;
+          })}
+        <div className="meme-button" onClick={() => {
+            this.setState({
+              memeLimit: this.state.memeLimit + this.state.addMeme
+            });
+          }}>
+          Load {this.state.addMeme} more memes...
+        </div>
       </div>;
   }
 }
 
 export default App;
+
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import Search from './components/Search'
+// import MemesList from './components/MemesList'
+// import DetailView from './components/DetailView';
+// import Meme from './components/Meme'
+
+
+
+// const url = "https://api.imgflip.com/get_memes"
+// const captionUrl = "https://api.imgflip.com/caption_image";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       searchTerm: "",
+//       memes: [],
+//       featuredMeme: null,
+//       memeText: ''
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.fetchMemes();
+//   }
+//   fetchCaptionMeme = () => {
+//     fetch(captionUrl).then(resp=>resp.json()).then(result => this.setState({featuredMeme: result}))
+//   }
+//   fetchMemes = () => {
+//     fetch(url)
+//       .then(resp => resp.json())
+//       .then(result => this.setState({ memes: result.data.memes }));
+//     //
+//   };
+
+//   updateTermSearch = newSearchTerm => {
+//     this.setState({ searchTerm: newSearchTerm });
+//   };
+
+//   filterMemes = () => {
+//     return this.state.memes.filter(meme =>
+//       meme.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+//     );
+//   };
+
+//   setClickedMeme = (meme) => {
+//     this.setState({featuredMeme: meme})
+//   }
+
+//   changeMeme(text) {
+//     this.setState({
+//       memeText: text
+//     });
+//   }
+
+//   render() {
+//     return (
+//     <div className="App">
+//         <div>
+//           <h2>Meme Generator</h2>
+//           <input onChange={e => this.changeMeme(e.target.value)}
+//             autoFocus="1" />
+//           <Meme text={this.state.memeText} />
+//         </div>
+//     <div>
+//       {this.state.featuredMeme ? <DetailView meme={this.state.featuredMeme} /> : null}
+//         <Search updateTermSearch={this.updateTermSearch} />
+//         <MemesList filterMemes={this.filterMemes()} setClickedMeme={this.setClickedMeme} />
+//       </div>
+      
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+
+
+
--- meme-generator-client/src/components/MemesList.js
+++ meme-generator-client/src/components/MemesList.js
@@ -1,15 +1,53 @@
-import React from "react";
-import MemesListItem from './MemesListItem'
+import React from 'react';
 
-const MemesList = props => {
-    // console.log("props", props.meme.name)
-  return <div>
-      {props.filterMemes.map(meme => ( <MemesListItem meme={meme} key={meme.id} />
-      ))}
+
+class MemesList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hover: false
+    }
+  }
+
+  render() {
+    return <div className="meme-item" onMouseEnter={() => this.setState(
+      { hover: true }
+    )} onMouseLeave={() => this.setState({
+      hover: false
+      })} onClick={() => {
+        this.props.fetchClickedMeme(this.props.meme.id)
+      
+      console.log(this.props.meme.id)
+      }
+    }>
+      <img src={this.props.meme.url} alt={this.props.meme.name} className={this.state.hover ? "meme-img darken-img" : "meme-img"} />
+      <p className={this.state.hover ? "meme-txt" : "no-txt"}>
+        {this.props.meme.name}
+      </p>
     </div>;
+  }
 };
 
-export default MemesList; 
+export default MemesList;
+
+// import React from "react";
+// import MemesListItem from './MemesListItem'
+
+// const MemesList = props => {
+//     // console.log("props", props.meme.name)
+//   return <div>
+//       {props.filterMemes.map(meme => (
+//         <MemesListItem
+//           meme={meme}
+//           key={meme.id}
+//           setClickedMeme={props.setClickedMeme}
+//         />
+//       ))}
+//     </div>;
+// };
+
+// export default MemesList; 
 
 
 
